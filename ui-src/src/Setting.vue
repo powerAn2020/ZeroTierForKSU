f5b4cdf696933e52bcd75a6f6070378091d9bac2
powerAn <25736019+powerAn2020@users.noreply.github.com>
2024-04-18 15:19:32 +0800

    路由模式显示有问题

--- ui-src/src/Setting.vue
+++ ui-src/src/Setting.vue
@@ -59,7 +59,7 @@ const onSelect = (action) => {
   defaultRoterMode.value = action.text;
   showToast(action.text)
   console.info(action)
-  if (action.text === '1') {
+  if (action.text == '1') {
     console.info('main表优先模式')
     execCmd('rm /data/adb/zerotier/ROUTER_RULE_NEW').then(v => {
       console.info(v)
