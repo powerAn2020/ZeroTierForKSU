c2ec640d90f72c3e87599c757c5f16ed0182367c
powerAn <25736019+powerAn2020@users.noreply.github.com>
2025-09-25 17:15:39 +0800

    更新zerotier到1.16.0
    1. 添加action.sh ，用于切换服务状态
    2. 修复流水线
    3. 更新zerotier到1.16.0

--- ui-src/src/main.js
+++ ui-src/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import JsonViewer from "vue3-json-viewer";
-import "vue3-json-viewer/dist/index.css";
+import "vue3-json-viewer/dist/vue3-json-viewer.css";
 import { createPinia } from 'pinia'
 import router from './router'
 import { i18n, vantLocales } from './locales'; // 导入所有翻译信息
